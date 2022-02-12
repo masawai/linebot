81b3a4d5ffb4592b3212f54703d89e406519c9db
masawai <secretariat.tmopera@gmail.com>
2022-02-12 00:34:39 +0000

    Change to broadcast

--- linebot_push/dynamo_action.py
+++ linebot_push/dynamo_action.py
@@ -1,23 +1,28 @@
 import boto3
 import datetime
+import os
 from boto3.dynamodb.conditions import Key
 
 # DynamoDB
 dynamodb = boto3.resource('dynamodb')
 table    = dynamodb.Table('linebot')
 
+names = os.environ.get('names', 'Masaya,Mayu').split(',')
+
 def query_item(event=None):
     # This code will be executed at the beginning of the month.
     # The month of last day is last month
     last_day = datetime.datetime.now() - datetime.timedelta(days=1)
     year_month = last_day.strftime('%Y/%-m')
-    res = table.query(
-        KeyConditionExpression=Key('name').eq('Masaya') & Key('date').begins_with(year_month)
-    )
     text = ''
-    total_payment = 0
-    for row in res['Items']:
-        text += f"{row['description']} {row['price']}円\n" 
-        total_payment += row['price']
-    text += f"{last_day.month}月の合計：{total_payment}円"
+    for name in names:
+        res = table.query(
+            KeyConditionExpression=Key('name').eq(name) & Key('date').begins_with(year_month)
+        )
+        total_payment = 0
+        for row in res['Items']:
+            text += f"{row['date'][:-9]} {row['description']} {row['price']}円\n" 
+            total_payment += row['price']
+        text += f'{name}の{last_day.month}月の合計：{total_payment}円\n'
+    text += '忘れずに精算してね^_^'
     return text 
--- linebot_push/lambda_function.py
+++ linebot_push/lambda_function.py
@@ -11,16 +11,15 @@ logger = logging.getLogger(__name__)
 #SSM
 ssm = boto3.client('ssm')
 access_token = ssm.get_parameter(Name='/linebot/access_token',WithDecryption=True)['Parameter']['Value']
-masawai = ssm.get_parameter(Name='/linebot/user_id',WithDecryption=True)['Parameter']['Value']
+# user_id_list = ssm.get_parameter(Name='/linebot/user_id_list',WithDecryption=True)['Parameter']['Value'].split(',')
 
 line_bot_api = LineBotApi(access_token)
 
 
 def lambda_handler(event, context):
-    user_id = masawai
     text = dynamo_action.query_item(event)
     messages = TextSendMessage(text=text)
-    line_bot_api.push_message(user_id, messages=messages)
+    line_bot_api.broadcast(messages=messages)
 
     return {
         'statusCode': 200,
