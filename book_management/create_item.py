de1faa06cbb9883b3853ab1e7dda208d06ba962e
masawai <secretariat.tmopera@gmail>
2022-09-18 03:24:36 +0000

    画像のイメージがない場合の処理を追加

--- book_management/create_item.py
+++ book_management/create_item.py
@@ -7,6 +7,8 @@ import requests
 from notion_client import Client
 
 logger = logging.getLogger(__name__)
+logLevel=logging.DEBUG
+logger.setLevel(logLevel)
 
 #SSM
 ssm = boto3.client('ssm')
@@ -19,35 +21,41 @@ notion = Client(auth=notion_token)
 
 def get_bibliograph_by_isbn(isbn):
     r = requests.get(f'https://api.openbd.jp/v1/get?isbn={isbn}')
-    return r.json()[0]['summary'] if r.json() else False
+    return r.json()[0]['summary'] if r.json() else None
 
 def parse_item(bibliograph=None):
+    title = bibliograph.get('title')
+    author = bibliograph.get('author')
+    publisher = bibliograph.get('publisher')
+    pubdate = bibliograph.get('pubdate')
+    cover =  bibliograph.get('cover') if bibliograph.get('cover') else 'https://www.shoshinsha-design.com/wp-content/uploads/2020/05/noimage-1-760x460.png'
+    isbn = bibliograph.get('isbn')
     properties = {
         'Name': {
             'title': [{
                 'text': {
-                    'content': bibliograph['title']
+                    'content': title
                 }
             }]
         },
         'Author': {
             'rich_text': [{
                 'text': {
-                    'content': bibliograph['author']
+                    'content': author
                 }
             }]
         },
         'Publisher': {
             'rich_text': [{
                 'text': {
-                    'content': bibliograph['publisher']
+                    'content': publisher
                 }
             }]
         },
         'Pubdate': {
             'rich_text': [{
                 'text': {
-                    'content': bibliograph['pubdate']
+                    'content': pubdate
                 }
             }]
         },
@@ -56,14 +64,14 @@ def parse_item(bibliograph=None):
                 'name': "image url",
                 'type' : "external",
                 'external':{
-                    'url' : bibliograph['cover']
+                    'url' : cover
               }
             }]
         },
         'Isbn': {
             'rich_text': [{
                 'text': {
-                    'content': bibliograph['isbn']
+                    'content': isbn
                 }
             }]
         },
@@ -72,6 +80,7 @@ def parse_item(bibliograph=None):
 
 def create_item(bibliograph):
     properties=parse_item(bibliograph)
+    logger.info(properties)
     r = notion.pages.create(
         **{
             'parent': {'database_id' : database_id},
